83f65cc5c7fff834884f0a3eec5d66b020029f2e
Asbjørn M Bonvik <ambonvik@gmail.com>
2026-04-19 00:03:01 +0200

    Test harness for firing asserts, initial commit

--- test/test_assert.py
+++ test/test_assert.py
@@ -1,8 +1,20 @@
-import unittest
+# Test harness for asserts, where a non-zero exit code may (or may not) be a "success".
+# Usage: python test_assert.py <expected_trip: true | false> <executable> [args...]
+import subprocess
+import sys
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+should_trip = sys.argv[1].lower() == "true"
+cmd = sys.argv[2:]
 
-if __name__ == '__main__':
-    unittest.main()
+try:
+    result = subprocess.run(cmd, capture_output=True)
+    did_trip = result.returncode != 0
+    if should_trip == did_trip:
+        sys.exit(0)
+    else:
+        print(f"Expected {should_trip}, got {did_trip}")
+        sys.exit(1)
+
+except Exception as e:
+    print(f"Failed to execute test: {e}")
+    sys.exit(1)
