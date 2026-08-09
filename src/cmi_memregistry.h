f6c23f5cb178405e12c46260c6917c55031f8200
Asbjørn M Bonvik <ambonvik@gmail.com>
2026-08-09 22:11:31 +0200

    Added predicate function to indicate teardown in progress

--- src/cmi_memregistry.h
+++ src/cmi_memregistry.h
@@ -22,8 +22,6 @@
 #ifndef CIMBA_CMI_MEMREGISTRY_H
 #define CIMBA_CMI_MEMREGISTRY_H
 
-#include <stdbool.h>
-
 #include "cmi_config.h"
 #include "cmi_dlist.h"
 
@@ -36,16 +34,33 @@ typedef void (cmi_teardown_func)(void *obj);
 
 /* The registry tag, embedded in the managed objects */
 struct cmi_memregistry_item {
-    cmi_teardown_func  *teardown;
-    cmi_dlist_node node;
+    cmi_teardown_func *teardown;   /* Destructor function */
+    void *object;                   /* Start address of the object to be demolished */
+    struct cmi_dlist_node node;     /* Registry list links */
 };
 
 /* The actual registry of memory objects */
 extern CMB_THREAD_LOCAL struct cmi_dlist_node cmi_memregistry;
-extern CMB_THREAD_LOCAL bool cmi_memregistry_armed;
 
-extern void cmi_memregistry_add(void *obj, cmi_teardown_func *fn);
-extern void cmi_memregistry_remove(void *obj);
+/*
+ * Add an object to the registry, pushing it from the head of the list.
+ */
+extern void cmi_memregistry_add(struct cmi_memregistry_item *item, cmi_teardown_func *fn);
+
+/*
+ * Remove a given object from the registry.
+ */
+extern void cmi_memregistry_remove(struct cmi_memregistry_item *item);
+
+/*
+ * Execute the teardown in LIFO order for proper create, initialize,
+ * terminate, destroy sequence.
+ */
 extern void cmi_memregistry_teardown(void);
+
+/*
+ * Predicate function to identify if a teardown is in progress or not.
+ */
+extern bool cmi_memregistry_is_demolishing(void);
 
 #endif /* CIMBA_CMI_MEMREGISTRY_H */
