82a48a090e5f42d86cff6984dcc09cba42a0b581
Asbjørn M Bonvik <ambonvik@gmail.com>
2026-09-08 23:23:46 +0200

    Removed ban against zero seed value

--- test/test_cimba.c
+++ test/test_cimba.c
@@ -39,7 +39,7 @@
 #include <unistd.h>
 
 #include "cimba.h"
-#include "test.h"
+#include "testutils.h"
 
 #define USERFLAG1 0x00000001
 #define USERFLAG2 0x00000002
@@ -417,7 +417,7 @@ int main(const int argc, char **argv)
             case 's': {
                 errno = 0;
                 seed = (uint64_t)strtoull(optarg, NULL, 0);
-                if (errno != 0 || seed == 0u) {
+                if (errno != 0) {
                     fprintf(stderr, "Invalid argument %s\n", optarg);
                     abort();
                 }
