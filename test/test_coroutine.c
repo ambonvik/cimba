836427d99b23c96abb5e082b4308a51d5519e303
Asbjørn M Bonvik <ambonvik@gmail.com>
2026-09-08 23:24:43 +0200

    Renamed test.h to testutils.h

--- test/test_coroutine.c
+++ test/test_coroutine.c
@@ -22,7 +22,7 @@
 
 #include "cmi_coroutine.h"
 #include "cmi_memutils.h"
-#include "test.h"
+#include "testutils.h"
 
 /* Simple test function, just a single event that returns */
 static void *corofunc(struct cmi_coroutine *myself, void *context)
